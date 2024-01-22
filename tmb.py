6660e9ae90cd289c72e3a2bea0c31d8225a27ea0
matheusymm <89025559+matheusymm@users.noreply.github.com>
2024-01-22 14:13:14 -0300

    Update tmb.py

--- tmb.py
+++ tmb.py
@@ -2,38 +2,38 @@ import math
 import unittest
 
 class MetabolismoBasal():
-    def __init__(self, fatAtividade, peso, altura, idade, genero):
+    def __init__(self, fatAtividade, peso, altura, idade, sexo):
         self.fatAtividade = fatAtividade
         self.peso = peso
         self.altura = altura
         self.idade = idade
-        self.genero = genero
+        self.sexo = sexo
     
-    def calcMasc(self):
+    def calcHomem(self):
         return math.ceil(self.fatAtividade*(66 + 13.7*self.peso + 5*self.altura - 6.8*self.idade))
     
-    def calcFem(self):
+    def calcMulher(self):
         return math.ceil(self.fatAtividade*(655 + 9.6*self.peso + 1.8*self.altura - 4.7*self.idade))
     
 class TesteClass(unittest.TestCase):
-    def testeMascSedentario(self):
+    def testeHomemSedentario(self):
         TMB = MetabolismoBasal(1.2, 60, 170, 24, 0)
-        self.assertEqual(1890, TMB.calcMasc(), "Masculino Sedentário Falhou!")
-    def testeMascModeradamenteAtivo(self):
+        self.assertEqual(1890, TMB.calcHomem(), "Homem Sedentário Falhou!")
+    def testeHomemModeradamenteAtivo(self):
         TMB = MetabolismoBasal(1.55, 60, 170, 24, 0)
-        self.assertEqual(2441, TMB.calcMasc(), "Masculino Moderadamente Ativo Falhou!")
-    def testeMascAltamenteAtivo(self):
+        self.assertEqual(2441, TMB.calcHomem(), "Homem Moderadamente Ativo Falhou!")
+    def testeHomemAltamenteAtivo(self):
         TMB = MetabolismoBasal(1.725, 60, 170, 24, 0)
-        self.assertEqual(2717, TMB.calcMasc(), "Masculino Sedentário Falhou!")
-    def testeFemSedentario(self):
+        self.assertEqual(2717, TMB.calcHomem(), "Homem Sedentário Falhou!")
+    def testeMulherSedentario(self):
         TMB = MetabolismoBasal(1.2, 60, 170, 24, 1)
-        self.assertEqual(1710, TMB.calcFem(), "Feminino Sedentário Falhou!")
-    def testeFemModeradamenteAtivo(self):
+        self.assertEqual(1710, TMB.calcMulher(), "Mulher Sedentária Falhou!")
+    def testeMulherModeradamenteAtivo(self):
         TMB = MetabolismoBasal(1.55, 60, 170, 24, 1)
-        self.assertEqual(2208, TMB.calcFem(), "Feminio Moderadamente Ativo Falhou!")
-    def testeFemAltamenteAtivo(self):
+        self.assertEqual(2208, TMB.calcMulher(), "Mulher Moderadamente Ativa Falhou!")
+    def testeMulherAltamenteAtivo(self):
         TMB = MetabolismoBasal(1.725, 60, 170, 24, 1)
-        self.assertEqual(2457, TMB.calcFem(), "Feminino Altamente Ativo Falhou!")
+        self.assertEqual(2457, TMB.calcMulher(), "Mulher Altamente Ativa Falhou!")
 
 if __name__ == "__main__":
     unittest.main()
